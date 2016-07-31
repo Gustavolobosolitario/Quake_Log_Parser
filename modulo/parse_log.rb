3706b368e79863a465a40d26d5599035ab615af3
Felipe Silva <felipe_96sp@msn.com>
2016-07-31 19:22:19 -0300

    Criação de métodos (na Classe 'ParseLog') para usar as regras de inicio e fim de Jogo

--- modulo/parse_log.rb
+++ modulo/parse_log.rb
@@ -1,13 +1,21 @@
  module ParseLog
 
-	#regra para identificar o inicio do jogo
+	#regra para identificar o inicio de jogo
 	def self.inicio_jogo_regra
     	/InitGame/
   	end
 
-  	#regra para identificar o fim do jogo
+  	#regra para identificar o fim de jogo
   	def self.fim_jogo_regra
     	/--------+/
   	end
 
+  	#metodos para usar as regras
+	def self.inicio_jogo_linha?(linha_log)
+    	linha_log =~ ParseLog.inicio_jogo_regra
+  	end
+  
+	def self.fim_jogo_linha?(linha_log)
+		linha_log =~ ParseLog.fim_jogo_regra
+	end
 end
