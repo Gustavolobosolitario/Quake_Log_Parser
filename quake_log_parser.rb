f20cfcf9bd9f52a2a351d79d0eec9e7bd8097016
Felipe Silva <felipe_96sp@msn.com>
2016-07-31 19:00:48 -0300

    Criação do método 'ler_arquivo' na classe 'QuakeLogParse'

--- quake_log_parser.rb
+++ quake_log_parser.rb
@@ -1,3 +1,13 @@
 class QuakeLogParser
+	
+	#Método para ler arquivo e retorna-lo em um array
+	def self.ler_arquivo (arquivo = 'games.log') #Definindo arquivo de log padrão.
+      linhas_array = []
+      arquivo = File.open(arquivo, 'r')
+      #Atribuindo linhas do arquivo no array
+      arquivo.each_line {|linha| linhas_array << linha}
+
+	  linhas_array
+	end
 
 end
