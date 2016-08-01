2da996fbcf7971913dce978ae7379887d9eeaa71
Felipe Silva <felipe_96sp@msn.com>
2016-08-01 20:28:23 -0300

    Criação da regra para identificar morte(Kill:) no modulo ParseLog

--- modulo/parse_log.rb
+++ modulo/parse_log.rb
@@ -1,21 +1,30 @@
  module ParseLog
 
-	#regra para identificar o inicio de jogo
-	def self.inicio_jogo_regra
-    	/InitGame/
-  	end
+  #regra para identificar o inicio de jogo
+  def self.inicio_jogo_regra
+    /InitGame/
+  end
 
-  	#regra para identificar o fim de jogo
-  	def self.fim_jogo_regra
-    	/--------+/
-  	end
+  #regra para identificar o fim de jogo
+  def self.fim_jogo_regra
+    /--------+/
+  end
 
-  	#metodos para usar as regras
-	def self.inicio_jogo_linha?(linha_log)
-    	linha_log =~ ParseLog.inicio_jogo_regra
-  	end
-  
-	def self.fim_jogo_linha?(linha_log)
-		linha_log =~ ParseLog.fim_jogo_regra
-	end
+  #regra para verificar o numero de mortes (kill) por jogo
+  def self.morte_regra
+    /Kill:/
+  end
+
+  #metodos para usar as regras
+  def self.inicio_jogo_linha?(linha_log)
+    linha_log =~ ParseLog.inicio_jogo_regra
+  end
+
+  def self.fim_jogo_linha?(linha_log)
+    linha_log =~ ParseLog.fim_jogo_regra
+  end
+
+  def self.morte_linha?(linha_log)
+    linha_log =~ ParseLog.kill_regra
+  end
 end
