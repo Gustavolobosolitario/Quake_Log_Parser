a961dd96cc33b12c793b804cd22c64352b749796
Felipe Silva <felipe_96sp@msn.com>
2016-08-02 00:24:04 -0300

    Implementação da Classe Jogo(metodos initialize e to_s), retornando nome do game(ex:game_1) e total de mortes(total_kills)

--- modelos/jogo.rb
+++ modelos/jogo.rb
@@ -1,3 +1,23 @@
+require_relative '../modulo/parse_log'
 class Jogo
+
+	attr_accessor :game, :total_kills
+
+	def initialize(linhas_log_array, game)
+
+		@game = game
+		@total_kills = 0
+
+		linhas_log_array.each do |linha_log|
+			if ParseLog.morte_linha?(linha_log)
+				@total_kills += 1
+			end
+		
+		end
+	end
+
+	def to_s
+		"#{self.game} : { total_kills: #{self.total_kills}}"
+	end
 
 end
